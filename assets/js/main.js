c320024d3fa82429f748179dfd1476251c346842
dfsdanimaulana <danimaulana9f@gmail.com>
2023-02-27 11:06:52 +0700

    skills grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,22 +151,6 @@
     });
   }
 
-  /**
-   * Skills animation
-   */
-  let skilsContent = select('.skills-content');
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
 
   /**
    * Portfolio isotope and filter
